a8dafbd6dcefc2c1485ab91432768db1dd168f2a
linconl-cmd <linconl.lee@gmail.com>
2024-09-10 20:39:19 -0300

    update

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-const fs = require('fs');
+import express from 'express';
+import cors from 'cors';
+import { promises as fs } from 'fs';
+
 const app = express();
-
 const PORT = process.env.PORT || 3000;
-
-
-app.get("/", (req, res) => {
-    return res.json("hello world")
-})
-
-// Iniciar o servidor
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em {PORT}`);
-});
 
 // Habilitar CORS para todas as origens
 app.use(cors());
@@ -21,27 +11,34 @@ app.use(cors());
 // Middleware para interpretar JSON
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    return res.json("hello world");
+});
+
 // Rota para salvar a seleção
-app.post('/save-selection', (req, res) => {
+app.post('/save-selection', async (req, res) => {
     const selection = req.body;
 
-    // Salvar a seleção em um arquivo db.json
-    fs.writeFile('db.json', JSON.stringify(selection, null, 2), (err) => {
-        if (err) {
-            return res.status(500).json({ message: 'Erro ao salvar a seleção' });
-        }
+    try {
+        // Salvar a seleção em um arquivo db.json
+        await fs.writeFile('db.json', JSON.stringify(selection, null, 2));
         res.json({ message: 'Seleção salva com sucesso' });
-    });
+    } catch (err) {
+        return res.status(500).json({ message: 'Erro ao salvar a seleção' });
+    }
 });
 
 // Rota para carregar a seleção
-app.get('/get-selection', (req, res) => {
-    fs.readFile('db.json', 'utf8', (err, data) => {
-        if (err) {
-            return res.status(500).json({ message: 'Erro ao carregar a seleção' });
-        }
+app.get('/get-selection', async (req, res) => {
+    try {
+        const data = await fs.readFile('db.json', 'utf8');
         res.json(JSON.parse(data));
-    });
+    } catch (err) {
+        return res.status(500).json({ message: 'Erro ao carregar a seleção' });
+    }
 });
 
-
+// Iniciar o servidor
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em ${PORT}`);
+});
