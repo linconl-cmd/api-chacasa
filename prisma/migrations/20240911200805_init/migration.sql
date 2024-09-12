6582ae4f2f6c9cabc4ef5a61bdb09cd66a548ed7
linconl-cmd <linconl.lee@gmail.com>
2024-09-11 21:45:13 -0300

    updt

--- prisma/migrations/20240911200805_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Selection" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "item" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Selection_pkey" PRIMARY KEY ("id")
-);
